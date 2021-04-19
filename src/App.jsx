40d6d4e6b9677d4d104d819c160b9af0e516d411
sobrien007 <sobrien@banyanlabs.io>
2021-04-19 16:37:11 -0700

    little change after context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useContext} from 'react';
+import React from 'react';
 // import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import './vender/font-awesome/css/all.css'
 import Header from './components/Header';
 import Main from './components/Main';
-import {MainContext} from './components/Context';
+
 
 
 
@@ -13,7 +13,7 @@ import {MainContext} from './components/Context';
 
 
 const App = () => {
-const store = useContext(MainContext);
+
 
   return (
     <div  className="App " >
