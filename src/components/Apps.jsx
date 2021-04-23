4a3b579f23e486dfa90ac3c16cebb89ba15d5092
sean obrien <ecosean1@gmail.com>
2021-04-23 10:46:19 -0700

    update links

--- src/components/Apps.jsx
+++ src/components/Apps.jsx
@@ -10,7 +10,7 @@ const Apps = () => {
         <div className="w3-row w3-padding-32">
       <div className="w3-third" style={{filter: `hue-rotate(-${store.hueRotate}deg)`}}>
         <h5>Cool Blue</h5>
-       <a href='https://github.com/sobrien007/coolblue'> <img src="assets/img/coolBlueScreenShot.png" className="w3-round w3-margin-bottom" alt="cool blue"  style={{'width':90 + '%'}}></img></a>
+       <a href='https://thirsty-kilby-a72701.netlify.app/'> <img src="assets/img/coolBlueScreenShot.png" className="w3-round w3-margin-bottom" alt="cool blue"  style={{'width':90 + '%'}}></img></a>
         <p>This site features a light/dark theme as well as three other themes. Built with HTML/CSS and jQuery/Ajax. The messaging system is simple and easy for the client to use.</p>
       </div>
       <div className="w3-third" style={{filter: `hue-rotate(-${store.hueRotate}deg)`}}>
@@ -20,7 +20,7 @@ const Apps = () => {
       </div>
       <div className="w3-third" style={{filter: `hue-rotate(-${store.hueRotate}deg)`}}>
         <h5>Healthy Living</h5>
-       <a href='https://github.com/sobrien007/healthyLiving'> <img src="assets/img/healthyLiving.png" className="w3-round" alt="healthy living website" style={{'width':90 + '%'}}></img></a>
+       <a href='https://nostalgic-bhaskara-d714a4.netlify.app/'> <img src="assets/img/healthyLiving.png" className="w3-round" alt="healthy living website" style={{'width':90 + '%'}}></img></a>
         <p>Healthy Living is a site that uses HTML, CSS and jQuery/Ajax. </p>
       </div>
     </div>
