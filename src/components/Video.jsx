e69953ba08dd3a1dfbb6b351dda5e31d84d270ee
sobrien-banyan <sean.obrien.webdeveloper@gmail.com>
2022-03-21 09:53:18 -0700

    github contributions march

---
+++ src/components/Video.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from "react";
+import { MainContext } from "./Context";
+
+const Video = ({ videoSourceMp4 }) => {
+    const store = useContext(MainContext);
+    return (
+        <div style={{ filter: `hue-rotate(-${store.hueRotate}deg)` }}>
+            <video height="240" width="320" preload="auto" autoplay="autoplay" controls="controls" muted="">
+                <source src={videoSourceMp4} type="video/mp4" />
+            </video>
+        </div>
+    );
+};
+
+export default Video;
