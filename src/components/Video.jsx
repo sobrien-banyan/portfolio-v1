4d554d5e601861d430f2ad8c516843eb40bb8862
sobrien-banyan <sobrien@banyanlabs.io>
2022-04-24 16:14:07 -0700

    muted attribute on video tag

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,7 +5,7 @@ const Video = ({ videoSourceMp4 }) => {
     const store = useContext(MainContext);
     return (
         <div style={{ filter: `hue-rotate(-${store.hueRotate}deg)` }}>
-            <video height="240" width="320" preload="auto" autoplay="autoplay" controls="controls" muted="">
+            <video height="240" width="320" preload="auto" autoplay="autoplay" controls="controls" muted>
                 <source src={videoSourceMp4} type="video/mp4" />
             </video>
         </div>
