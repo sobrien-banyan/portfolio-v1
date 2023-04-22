90d5872c6a1cfdbbe640846beb55dde3f8c85293
sobrien-banyan <sobrien@banyanlabs.io>
2023-04-21 18:20:09 -0700

    repeat cards

--- src/components/application-carroussel/index.js
+++ src/components/application-carroussel/index.js
@@ -25,16 +25,20 @@ export default function ApplicationCarroussel() {
     },
     {
       key: uuidv4(),
-      content: <CardNews url='' discription='' title='' link='' />
+      content: <CardNews url='assets/img/ondemand-SH.png' discription='Ondemand by SimplyHired offers an assortment of web services. Built with React and styled-component.' title='Ondemand by SimplyHired' link='https://ondemand.simplyhired.com/' />
     },
     {
       key: uuidv4(),
-      content: <CardNews url='' discription='' title='' link='' />
+      content: <CardNews url='assets/img/coolBlueScreenShot.png' discription='First built with HTML/CSS and jQuery then converted to React. Frontend only' title='Cool Blue' link='https://cool-blue.netlify.app/' />
     },
     {
       key: uuidv4(),
-      content: <CardNews url='' discription='' title='' link='' />
-    }
+      content: <CardNews url='assets/img/healthyLiving.png' discription='This site features a light/dark theme as well as three other themes. Healthy Living is a site that was first built using HTML, CSS and jQuery then converted to React.' title='Healthy Living' link='https://healthy-living-jo.netlify.app/' />
+    },
+    {
+      key: uuidv4(),
+      content: <CardNews url='assets/img/persevereLMS1.jpg' discription='In this project we used MVC for the file structure and React for the Front-end framework. Context/API was used for state management. Express,Mongoose and Node.JS on the back-end. The databases used were MariaBD(SQL) and MongoDB. Being the project manager on this was a great opportunity. I contributed to both the Front-end and Back-end code. This was a huge project that took months to complete.' title='Persevere LMS' link='https://github.com/sobrien-banyan/persevere-lms' />
+    },
   ]; /*.map((element, index) => {
     return { ...element, onClick: () => setGoToSlide(index) };
   });*/
